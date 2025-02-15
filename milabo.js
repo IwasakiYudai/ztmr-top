177aeea7dc515d48861fc840b681a2ea7e8291bc
iwasakiyudai <yudai1213iwasaki@gmail.com>
2025-02-16 01:15:27 +0900

    add milabo page

---
+++ milabo.js
@@ -0,0 +1,8 @@
+const videoIDs = [
+    "I88PrE-KUPk?si=FNudarP0yvEdtj0G",
+];
+const randomIndex = Math.floor(Math.random() * videoIDs.length);
+const chosenID = videoIDs[randomIndex];
+
+const iframe = document.getElementById("youtube-iframe");
+iframe.src = "https://www.youtube.com/embed/" + chosenID + "?autoplay=1&mute=1";
